d63ec640997102e540c2338e56c0ca322cf6e00f
mjball98 <mjball200@gmail.com>
2021-03-02 23:28:02 -0500

    Formatting html pages, and adding some tests

--- TRMS/src/test/java/com/revature/reimbursementtest/ReimbursementServiceTest.java
+++ TRMS/src/test/java/com/revature/reimbursementtest/ReimbursementServiceTest.java
@@ -2,6 +2,7 @@ package com.revature.reimbursementtest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -35,12 +36,51 @@ public class ReimbursementServiceTest {
 
 	@Test
 	public void testReimbursementServiceFindAll() {
-		ArrayList<Reimbursement> myReimbursements = new ArrayList<>(Arrays.asList(new Reimbursement(1, 2000, 3, "denied", "none", 1),
-				new Reimbursement(12, 350, 1, "pending", "none", 0), new Reimbursement(3, 99999, 15, "approved","none",2)));
+		ArrayList<Reimbursement> myReimbursements = new ArrayList<>(Arrays.asList(
+				new Reimbursement(1, 2000, 3, "denied", "none", 1), new Reimbursement(12, 350, 1, "pending", "none", 0),
+				new Reimbursement(3, 99999, 15, "approved", "none", 2)));
 
 		Mockito.when(reimbursementRepositoryImpl.findAll()).thenReturn(myReimbursements);
 
 		Assert.assertEquals(myReimbursements, reimbursementService.findAll());
+
+	}
+
+	@Test
+	public void testReimbursementFindById() {
+		ArrayList<Reimbursement> myReimbursements = new ArrayList<>(Arrays.asList(
+				new Reimbursement(1, 2000, 3, "denied", "none", 1), new Reimbursement(12, 350, 1, "pending", "none", 0),
+				new Reimbursement(3, 99999, 15, "approved", "none", 2)));
+
+		Mockito.when(reimbursementRepositoryImpl.findAll()).thenReturn(myReimbursements);
+
+		Reimbursement returnedReimbursement = reimbursementService.findById(3);
+
+		for (Reimbursement r : myReimbursements) {
+			if (r.getId() == 3) {
+				Assert.assertEquals(r.getEmployee_id(), returnedReimbursement.getEmployee_id());
+			}
+		}
+
+	}
+
+	@Test
+	public void testReimbursementfindAllPendingForEmployeebyId() {
+		ArrayList<Reimbursement> myReimbursements = new ArrayList<>(Arrays.asList(
+				new Reimbursement(1, 2000, 3, "denied", "none", 1), new Reimbursement(12, 350, 1, "pending", "none", 0),
+				new Reimbursement(3, 99999, 15, "approved", "none", 2)));
+
+		Mockito.when(reimbursementRepositoryImpl.findAll()).thenReturn(myReimbursements);
+
+		
+		
+		List<Reimbursement> reimbursements = reimbursementService.findAllPendingForEmployeeById(3);
+
+		for (Reimbursement r : myReimbursements) {
+			if (r.getId() == 3) {
+				Assert.assertEquals(r.getEmployee_id(), reimbursements.get(0).getEmployee_id());
+			}
+		}
 
 	}
 }
